658cd202ebbe41d5aa1d4899930be03f7f30b704
harish-kumar8703 <55269849+harish-kumar8703@users.noreply.github.com>
2020-06-03 15:40:46 +0530

    Update firebaseconfig.js

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -1,10 +1,11 @@
 export default  {
-    apiKey: "AIzaSyAsc8Cu1ZDadxCfmsZF0kq83gTD-7FUK7I",
-    authDomain: "woven-arcadia-271413.firebaseapp.com",
-    databaseURL: "https://woven-arcadia-271413.firebaseio.com",
-    projectId: "woven-arcadia-271413",
-    storageBucket: "woven-arcadia-271413.appspot.com",
-    messagingSenderId: "110908873990",
-    appId: "1:110908873990:web:a08999d9fa24008db25287",
-    measurementId: "G-X28BTKBG94"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
+//paste your firebase API details
